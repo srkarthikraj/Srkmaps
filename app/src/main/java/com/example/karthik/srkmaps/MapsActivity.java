5288758919b16dcd954340a68c295589798df617
srkarthikraj <srkarthikraj@gmail.com>
2016-03-13 17:06:45 +0530

    updated Lat Long

--- app/src/main/java/com/example/karthik/srkmaps/MapsActivity.java
+++ app/src/main/java/com/example/karthik/srkmaps/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(9.9, 78.1);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Madurai"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
